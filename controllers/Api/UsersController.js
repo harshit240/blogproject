6304ff2cfe03a117028b7c13c57d51465624375c
harshit240 <601himanshusahu@gmail.com>
2022-11-01 19:27:24 +0530

    .

--- controllers/Api/UsersController.js
+++ controllers/Api/UsersController.js
@@ -1,6 +1,41 @@
 const UserModel = require('../../models/User')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 class UsersController{
+
+    static Register = async (req, res) => {
+        //    console.log(req.body);
+        const { name, email, password, confirm_password } = req.body;
+        const user = await UserModel.findOne({ email: email });
+    
+        if (user) {
+            res.send({ status: "failed", message: "ᴛʜɪꜱ ᴇᴍᴀɪʟ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴇxɪᴛꜱ😓" });
+        } 
+        else {
+          if (name && email && password && confirm_password) {
+            if (password == confirm_password) {
+              try {
+                const hashpassword = await bcrypt.hash(password, 10);
+                const result = await UserModel({
+                  name: name,
+                  email: email,
+                  password: hashpassword,
+                });
+                await result.save();
+                res.send({ status: 201, message: "Registration Successfully!😓" });
+              }catch (err) {
+                console.log(err);
+              }
+            } else {
+                res.send({ status: "failed", message: "Password and Confirm_password doesn`t match😓" });
+            }
+          }
+          else {
+            res.send({ status: "failed", message: "All Fields are Required😓" });
+          }
+        }
+      };
 
 }
 
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,93 +1,85 @@
-const UserModel = require("../models/User")
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const UserModel = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-class UserController{
+class UserController {
+  static AdminRegister = async (req, res) => {
+    res.render("admin/register", { message: req.flash("error") });
+  };
 
-    static AdminRegister = async(req,res)=>{
-
-        res.render('admin/register',{message:req.flash('error')})
-
-    }
-
-    static Register=async(req,res)=>{
-        
+  static Register = async (req, res) => {
     //    console.log(req.body);
-    const{name,email,password,confirm_password}=req.body
-    const admin = await UserModel.findOne({email:email})
+    const { name, email, password, confirm_password } = req.body;
+    const admin = await UserModel.findOne({ email: email });
 
-    if(admin){
-        req.flash('error','Email already exists')
-        return res.redirect('/admin/register')
-    }
-    else{
-        if(name && email && password && confirm_password){
-            if(password == confirm_password){
-                try{
-                    const hashpassword = await bcrypt.hash(password,10)
-                    const result = await UserModel({
-                        name:name,
-                        email:email,
-                        password:hashpassword
-                    })
-                    await result.save()
-                    req.flash('message','Registration Successful! Do login!')
-                    return res.redirect('/login')
-                }catch(err){
-                    console.log(err);
-                }
-            }else{
-                req.flash('error','Password and Confirm password doesn`t match')
-                return res.redirect('/admin/register')
-            }
-        }
-        else{
-            req.flash('error','All Fields are Required')
-            return res.redirect('/admin/register')
-        }
-    }
-
-}
-
-    static Verify_login = async(req,res)=>{
-        try{
-            const{email,password}=req.body;
-            if(email && password){
-                const user = await UserModel.findOne({email:email})
-                // console.log(user.password);
-                if(user != null){
-                    const isMatched = await bcrypt.compare(password,user.password)
-                    if((user.email === email) && isMatched){
-                        //verfiy token
-                        const token = jwt.sign({ userId: user._id }, 'himanshu123');
-                        // console.log(token);
-                        res.cookie('token',token)
-                        res.redirect('/admin/dashboard')
-                    }else{
-                        req.flash('error','Email or Password is not valid')
-                        return res.redirect('/login')
-                    }
-                }else{
-                    req.flash('error','You are not a registered user')
-                    return res.redirect('/login')
-                }
-            }else{
-                req.flash('error','All Fields Required')
-                    return res.redirect('/login')
-            }
-        }catch(err){
+    if (admin) {
+      req.flash("error", "Email already exists");
+      return res.redirect("/admin/register");
+    } else {
+      if (name && email && password && confirm_password) {
+        if (password == confirm_password) {
+          try {
+            const hashpassword = await bcrypt.hash(password, 10);
+            const result = await UserModel({
+              name: name,
+              email: email,
+              password: hashpassword,
+            });
+            await result.save();
+            req.flash("message", "Registration Successful! Do login!");
+            return res.redirect("/login");
+          } catch (err) {
             console.log(err);
+          }
+        } else {
+          req.flash("error", "Password and Confirm password doesn`t match");
+          return res.redirect("/admin/register");
         }
+      } else {
+        req.flash("error", "All Fields are Required");
+        return res.redirect("/admin/register");
+      }
     }
-    
-    static Logout = async(req,res)=>{
-        try{
-            res.clearCookie('token')
-            res.redirect('/login')
-        }catch(err){
-            console.log(err);
-        }
-    }
-}
-module.exports=UserController
+  };
 
+  static Verify_login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (email && password) {
+        const user = await UserModel.findOne({ email: email });
+        // console.log(user.password);
+        if (user != null) {
+          const isMatched = await bcrypt.compare(password, user.password);
+          if (user.email === email && isMatched) {
+            //verfiy token
+            const token = jwt.sign({ userId: user._id }, "himanshu123");
+            // console.log(token);
+            res.cookie("token", token);
+            res.redirect("/admin/dashboard");
+          } else {
+            req.flash("error", "Email or Password is not valid");
+            return res.redirect("/login");
+          }
+        } else {
+          req.flash("error", "You are not a registered user");
+          return res.redirect("/login");
+        }
+      } else {
+        req.flash("error", "All Fields Required");
+        return res.redirect("/login");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  static Logout = async (req, res) => {
+    try {
+      res.clearCookie("token");
+      res.redirect("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
+module.exports = UserController;
