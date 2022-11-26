8d7be019cf21783530fbd8ea2995dd2137a9488e
harshit240 <601himanshusahu@gmail.com>
2022-11-26 19:49:09 +0530

    .

--- db/connectdb.js
+++ db/connectdb.js
@@ -4,14 +4,13 @@ const connectDB = () => {
   
   // return mongoose.connect('mongodb://localhost:27017/blog_project')
 
-  return mongoose.connect("mongodb+srv://Himanshu2409:Himanshu123@cluster0.8rkck54.mongodb.net/blog_upload?retryWrites=true&w=majority")
+  return mongoose.connect("mongodb+srv://blogweb:blogweb123@cluster0.myiwucg.mongodb.net/PersonalBlog?retryWrites=true&w=majority")
     .then(() => {
       console.log("connected sucessfully");
     })
     .catch((err) => {
       console.log(err);
     });
-    
-    
+  
 };
 module.exports = connectDB;
