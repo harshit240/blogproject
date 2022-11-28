f83950cd28cd1b184e6b72ee851ac967cc362930
harshit240 <601himanshusahu@gmail.com>
2022-11-28 16:45:46 +0530

    .

--- db/connectdb.js
+++ db/connectdb.js
@@ -4,7 +4,7 @@ const connectDB = () => {
   
   // return mongoose.connect('mongodb://localhost:27017/blog_project')
 
-  return mongoose.connect("mongodb+srv://blogweb:blogweb123@cluster0.myiwucg.mongodb.net/PersonalBlog?retryWrites=true&w=majority")
+  return mongoose.connect(process.env.DB_URL)
     .then(() => {
       console.log("connected sucessfully");
     })
